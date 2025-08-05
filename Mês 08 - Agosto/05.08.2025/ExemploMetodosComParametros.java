d9d45513ec5c0b80f69426f252e819c3c2eade9b
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-05 20:44:47 -0300

    adiciona exemplo de métodos com parâmetros

---
+++ Mês 08 - Agosto/05.08.2025/ExemploMetodosComParametros.java
@@ -0,0 +1,13 @@
+
+public class ExemploMetodosComParametros {
+	public static void main(String[] args) {
+
+		subtrairValores(4, 2);
+
+	}
+
+	public static void subtrairValores(int num1, int num2) {
+		System.out.println(num1 - num2);
+	}
+
+}
