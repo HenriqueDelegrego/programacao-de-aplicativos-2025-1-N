111394e82cb1485ab5e89f9dc5222195790b2226
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-07 21:31:56 -0300

    Adiciona Orientação a objetos

---
+++ Mês 08 - Agosto/07.08.2025/Exemplo Orientação a Objetos/App.java
@@ -0,0 +1,18 @@
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Veiculo v1 = new Veiculo();
+
+		v1.modelo = "Honda Civic";
+		v1.comprimento = 4.7;
+		v1.passageiros = 5;
+		v1.velMax = 200;
+		v1.cor = "Preto";
+
+		System.out.println(v1.modelo);
+
+	}
+
+}
