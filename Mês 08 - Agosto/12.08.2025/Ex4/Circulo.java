a1f553f8fad6ca61e89c2707697ccebbd5b9b0f8
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-12 21:37:09 -0300

    Adiciona Ex4

---
+++ Mês 08 - Agosto/12.08.2025/Ex4/Circulo.java
@@ -0,0 +1,10 @@
+
+public class Circulo {
+
+	double raio;
+
+	public double calcularArea() {
+		return Math.PI * Math.pow(raio, 2);
+	}
+
+}
---
+++ Mês 08 - Agosto/12.08.2025/Ex4/Ex4.java
@@ -0,0 +1,16 @@
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+
+		Circulo c1 = new Circulo();
+		c1.raio = 1;
+
+		Circulo c2 = new Circulo();
+		c2.raio = 8;
+
+		System.out.println(c2.calcularArea());
+
+	}
+
+}
