be648942b1410ed7eabefe224dd6de781a576d1e
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-19 19:22:36 -0300

    Adiciona Javadoc

--- Mês 08 - Agosto/19.08.2025/Ex7/Aeronave.java
+++ Mês 08 - Agosto/19.08.2025/Ex7/Aeronave.java
@@ -15,10 +15,25 @@ public class Aeronave {
 		this.queimaCombustivel = queimaCombustivel;
 	}
 
+	/**
+	 * Calcula a autonomia da aeronave.
+	 * 
+	 * A autonomia é calculada pela fórmula:
+	 * autonomia = capacidade de combustível / queima de combustível por hora
+	 * 
+	 * @return A autonomia da aeronave em horas.
+	 */
 	public double calcularAutonomia() {
 		return capCombustivel / queimaCombustivel;
 	}
 
+	/**
+	 * Calcula a distância máxima que a aeronave pode voar.
+	 * A distância máxima é calculada pela fórmula:
+	 * distância máxima = autonomia * velocidade máxima
+	 * 
+	 * @return A distância máxima que a aeronave pode voar em quilômetros.
+	 */
 	public double calcularDistanciaMaxima() {
 		return calcularAutonomia() * velMax;
 	}
