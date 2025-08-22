aa624f041881f4184ef87a7a702f6432b836580a
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-08-21 21:01:46 -0300

    Adiciona Ex8

---
+++ Mês 08 - Agosto/21.08.2025/Ex8/Contribuinte.java
@@ -0,0 +1,83 @@
+
+public class Contribuinte {
+
+	private String nome;
+	private String cpf;
+	private String uf;
+	private double rendaAnual;
+
+	public Contribuinte(String nome, String cpf, String uf, double rendaAnual) {
+		setNome(nome);
+		setCpf(cpf);
+		setUf(uf);
+		setRendaAnual(rendaAnual);
+	}
+
+	public double calcularImposto() {
+		if (rendaAnual >= 0 && rendaAnual <= 4000) {
+			return 0;
+		} else if (rendaAnual >= 4001 && rendaAnual <= 9000) {
+			return rendaAnual * 0.058;
+		} else if (rendaAnual >= 9001 && rendaAnual <= 25000) {
+			return rendaAnual * 0.15;
+		} else if (rendaAnual >= 25001 && rendaAnual <= 35000) {
+			return rendaAnual * 0.275;
+		} else {
+			return rendaAnual * 0.3;
+		}
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if (nome == null || nome.isBlank()) {
+			System.out.println("Erro, nome inválido");
+		} else {
+			this.nome = nome;
+		}
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		if (cpf == null || cpf.length() != 11) {
+			System.out.println("Erro, cpf inválido");
+		} else {
+			this.cpf = cpf;
+		}
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		if (uf == null || uf.length() != 2) {
+			System.out.println("Erro, uf inválida");
+		} else {
+			this.uf = uf;
+		}
+	}
+
+	public double getRendaAnual() {
+		return rendaAnual;
+	}
+
+	public void setRendaAnual(double rendaAnual) {
+		if (rendaAnual < 0) {
+			System.out.println("Erro, renda anual inválida");
+		} else {
+			this.rendaAnual = rendaAnual;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Contribuinte [nome=" + nome + ", cpf=" + cpf + ", uf=" + uf + ", rendaAnual=" + rendaAnual + "]";
+	}
+
+}
