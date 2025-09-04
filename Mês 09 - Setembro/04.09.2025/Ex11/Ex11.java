8fc88038d328e2752653ad67dd115efb5dc77e2f
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-09-04 19:07:11 -0300

    Adiciona Ex11

---
+++ Mês 09 - Setembro/04.09.2025/Ex11/Ex11.java
@@ -0,0 +1,18 @@
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+
+		Livro l1 = new Livro("Turma da mônica", false);
+
+		l1.devolver();
+
+		System.out.println(l1);
+
+		l1.devolver();
+
+		System.out.println(l1);
+
+	}
+
+}
