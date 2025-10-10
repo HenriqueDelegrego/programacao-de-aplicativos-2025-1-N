a9f73af5d815cc66a2791e5ce7b6c6ab98c4b37e
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-09 21:08:27 -0300

    Adiciona Ex1

---
+++ Mês 10 - Outubro/09.10.2025/Circulo.java
@@ -0,0 +1,44 @@
+
+public class Circulo {
+
+	private String cor;
+
+	private double raio;
+
+	public Circulo(String cor, double raio) {
+		setCor(cor);
+		setRaio(raio);
+	}
+
+	public double calcularArea() {
+		return Math.PI * Math.pow(raio, 2);
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		if (cor == null || cor.isBlank()) {
+			throw new IllegalArgumentException("Cor inválido");
+		}
+		this.cor = cor;
+	}
+
+	public double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(double raio) {
+		if (raio <= 0) {
+			throw new IllegalArgumentException("Raio inválido");
+		}
+		this.raio = raio;
+	}
+
+	@Override
+	public String toString() {
+		return "Circulo [cor=" + cor + ", raio=" + raio + "]";
+	}
+
+}
---
+++ Mês 10 - Outubro/09.10.2025/Ex1.java
@@ -0,0 +1,13 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+		Cilindro c1 = new Cilindro("Branco", 5, 2);
+
+		System.out.println(c1.calcularVolume());
+		System.out.println(c1.calcularArea());
+
+	}
+
+}
