cd22bc75e89b5de6472f6fdd5222e1e5ee011157
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2025-10-16 19:57:56 -0300

    Adiciona ex4

---
+++ Mês 10 - Outubro/16.10.2025/Ex4/Computador.java
@@ -0,0 +1,48 @@
+
+public class Computador implements Dispositivo {
+
+	private String modelo;
+
+	private boolean conectado;
+
+	public Computador(String modelo, boolean conectado) {
+		super();
+		this.modelo = modelo;
+		this.conectado = conectado;
+	}
+
+	@Override
+	public boolean ligar() {
+		return conectado;
+	}
+
+	@Override
+	public boolean desligar() {
+		return ligar();
+	}
+
+	@Override
+	public String obterStatus() {
+		if (ligar()) {
+			return "O computador está ligado";
+		}
+		return "O computador está desligado";
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public boolean isConectado() {
+		return conectado;
+	}
+
+	public void setConectado(boolean conectado) {
+		this.conectado = conectado;
+	}
+
+}
